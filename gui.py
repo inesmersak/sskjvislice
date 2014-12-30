0b0257597d8faa6a9ccec159738206ea16780362
Ines <inesmersak@gmail.com>
2014-12-30 20:41:33 +0100

    Dodala osnovno okno v gui.

---
+++ gui.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+
+class Aplikacija():
+    def __init__(self, master):
+        master.title('Vislice')
+
+        okvir = Frame(master)
+
+root = Tk()
+App = Aplikacija(root)
+root.mainloop()
