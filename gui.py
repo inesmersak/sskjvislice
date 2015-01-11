800684989ebdee8419cb325935433ae151c41410
Ines <inesmersak@gmail.com>
2015-01-11 11:29:01 +0100

    Cleaned up the gui a bit. Dodani različni framei za lepše prikazovanje widgetov.

--- gui.py
+++ gui.py
@@ -45,7 +45,7 @@ class Aplikacija():
         self.spaces = 2
 
         master.title('Vislice')
-        master.minsize(width=400, height=300)
+        master.minsize(width=380, height=300)
 
         # meni
         self.meni = Menu(master)
@@ -53,34 +53,38 @@ class Aplikacija():
         self.meni.add_command(label="Nova igra", command=self.nova_igra)
         self.meni.add_command(label="Zapri", command=quit)
 
-        okvir = Frame(master, width=600, height=600)
-        okvir.grid()
+        okvir = Frame(master)
+        okvir.grid(row=0, column=0)
 
         self.odkrito = StringVar()  # koliko besede je do zdaj odkrito
         self.odkrito.set(self.beseda.za_gui())
-        Label(okvir, textvariable=self.odkrito).grid(row=1, column=1, columnspan=2)
-
-        self.gumbi = []
-        for i, l in enumerate(self.abeceda):
-            b = Button(okvir, text=l, command=lambda x=i: self.preveri(x))
-            self.gumbi.append(b)
-            b.grid(row=i // 9 + 1, column=i % 9 + 3)
+        Label(okvir, textvariable=self.odkrito).grid(row=0, column=0)
 
         self.defin = StringVar()
-        Label(okvir, textvariable=self.defin).grid(row=2, column=1, columnspan=2)
+        Label(okvir, textvariable=self.defin).grid(row=1, column=0)
 
         self.novo = Button(okvir, text="Nova igra", command=self.nova_igra)
-        self.novo.grid(row=3, column=1, columnspan=2)
+        self.novo.grid(row=2, column=0)
 
-        self.platno = Canvas(okvir, width=200, height=200)
-        self.platno.grid(row=4, column=1, rowspan=3, columnspan=2)
+        tipkovnica = Frame(master)
+        tipkovnica.grid(row=0, column=1)
+        self.gumbi = []
+        for i, l in enumerate(self.abeceda):
+            b = Button(tipkovnica, text=l, command=lambda x=i: self.preveri(x))
+            self.gumbi.append(b)
+            b.grid(row=i // 9, column=i % 9)
+
+        self.platno = Canvas(master, width=200, height=200)
+        self.platno.grid(row=1, column=0)
         self.platno.background = PhotoImage(file=self.pot_do_slike())
         self.platno.create_image(0, 0, image=self.platno.background, anchor='nw')
 
+        okvir1 = Frame(master)
+        okvir1.grid(row=2, column=0)
         self.napacno = StringVar()
         self.napacno.set(self.beseda.preostali_poskusi)
-        Label(okvir, text="Preostali poskusi: ").grid(row=6, column=1)
-        Label(okvir, textvariable=self.napacno).grid(row=6, column=2)
+        Label(okvir1, text="Preostali poskusi: ").grid(row=0, column=0)
+        Label(okvir1, textvariable=self.napacno).grid(row=0, column=1)
 
 root = Tk()
 App = Aplikacija(root, reading_parsing.random_beseda(), abeceda())
